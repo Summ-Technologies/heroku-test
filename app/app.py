ebec732972d007e75add5fa5cce46efff5f181fc
Jared Hanson <jred0011@gmail.com>
2022-02-21 02:26:36 -0800

    app change and envvar test

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,4 +7,5 @@ app = Flask(__name__)
 
 @app.route("/hb")
 def hb():
-    return "App is alive, number 2"
+    test_envvar = os.environ.get("TEST_ENVVAR")
+    return f"App is alive, number 2, {test_envvar}"
